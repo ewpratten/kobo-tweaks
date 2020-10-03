da260b5c11e149f384d663a065bac95f94fa2106
Evan Pratten <ewpratten@gmail.com>
2020-10-03 11:38:07 -0400

    Add  a ws shortcut command

--- kt/usr/bin/kt.py
+++ kt/usr/bin/kt.py
@@ -6,10 +6,21 @@ import os
 KT_WORKSPACE = "/mnt/onboard/kt"
 
 def _workspace():
+    """Print the abs workspace path"""
     print(KT_WORKSPACE)
 
 def _workspace_open():
-    pass
+    """Create and open the workspace (a writable path)"""
+
+    # Ensure the workspace exists
+    if not os.path.exists(KT_WORKSPACE):
+        os.mkdirs(KT_WORKSPACE)
+        print("Created system workspace directory")
+    
+    # Move to that dir
+    os.chdir(KT_WORKSPACE)
+    print("Opened the KT workspace")
+    
 
 # Listing of commands to their actions
 cli_commands = {
@@ -24,4 +35,8 @@ if user_command in cli_commands:
     exit(0)
 else:
     print("kt {command}: Unknown command".format(command=user_command))
+
+    # Print a listing of commands
+    for command in cli_commands:
+        print("kt {command}".format(command=command))
     exit(1)
